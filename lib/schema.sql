d702a4e82451cb6f52fbae11400dd475c6ee79eb
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-03 01:43:33 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,17 +2,17 @@ CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id TEXT,
-  subgenre_id TEXT,
+  subgenre_id TEXT
 );
 
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
-  name TEXT,
+  name TEXT
 );
 
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-  name TEXT,
+  name TEXT
 );
 
 CREATE TABLE books (
